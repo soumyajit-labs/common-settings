e45394c22eec796e151691db7e7e0deb108e50ee
Soumyajit-x <soumyajitsi96@gmail.com>
2024-10-26 13:26:57 +0530

    Update secret-operator.py

--- secrets-appender/secret-operator.py
+++ secrets-appender/secret-operator.py
@@ -7,24 +7,27 @@ load_dotenv()
 
 region = sys.argv[1]
 repository = sys.argv[2]
+env = sys.argv[3]
+
+print('Region is set to {0} and the environment is set to {1}...'.format(region, env))
 
 def fetch(key):
-    print('Trying to fetch the credentials for {0} repository...')
+    print('Trying to fetch the credentials for {0} repository...'.format(repository))
     session = boto3.session.Session()
     client = session.client( service_name='secretsmanager', region_name=region )
     try:
         get_secret_value_response = client.get_secret_value( SecretId=key )
     except ClientError as e:
-        print('Something went wrong while fetching the credentials for {0} repository...')
+        print('Something went wrong while fetching the credentials for {0} repository...'.format(repository))
         if e.response['Error']['Code'] == 'ResourceNotFoundException':
             return 401
         else:
             return 400
 
-    print('Fetched the credentials for {0} repository successfully!')
+    print('Fetched the credentials for {0} repository successfully!'.format(repository))
     return get_secret_value_response['SecretString']
 
-def create_file(env, secret):
+def create_file(secret):
     file_path = 'src/main/resources/{0}-secured-properties.yaml'.format(env)
     try:
         with open(file_path, 'w') as file:
